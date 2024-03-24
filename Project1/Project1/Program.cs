9b6a98075509134522d8bfa9a60582ae3fc29bec
s24848 <s24848@pjwstk.edu.pl>
2024-03-24 19:43:24 +0100

    dodanie funkcji wyliczającej średnią

--- Project1/Project1/Program.cs
+++ Project1/Project1/Program.cs
@@ -1,6 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("commit1");
-Console.WriteLine("commit2");
-Console.WriteLine("commit3 ");
+public class Project1
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 10, 20, 30, 40, 50 };
+        double average = CalculateAverage(numbers);
+        Console.WriteLine("Średnia wartość w tablicy: " + average);
+    }
+
+    public static double CalculateAverage(int[] array)
+    {
+        int sum = 0;
+        foreach (int num in array)
+        {
+            sum += num;
+        }
+
+        return (double)sum / array.Length;
+    }
+}
